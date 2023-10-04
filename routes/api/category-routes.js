4bf518febad5c6c76ae56fc4b552755ecfcf1752
artorrias <sam.henson1119@outlook.com>
2023-10-03 22:15:44 -0600

    fixed finding by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const catData = await Category.findbyPk(req.params.id, {
+    const catData = await Category.findByPk(req.params.id, {
       include: [{ model: Product, attributes: ["product_name"] }],
     });
     res.status(200).json(catData);
